8920d7bab2f7ebb84de515dc345ee711d685d824
RanjeetSingh17 <ranjeetsingh71717@gmail.com>
2026-06-07 18:02:20 +0530

    Initial commit

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,9 +1,46 @@
+// const admin = require('firebase-admin');
+// admin.initializeApp({
+//   credential: admin.credential.cert(require('../config/service_key.json')),
+// });
+// const verifyToken = async (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//     return res.status(403).json({ error: 'Unauthorized' });
+//   }
+
+//   const token = authHeader.split('Bearer ')[1];
+
+//   try {
+//     const decodedToken = await admin.auth().verifyIdToken(token);
+//     const uid = decodedToken.uid;
+//     req.user = { uid };
+//     console.log("Reequesting userin the frontend is ",req.user)
+//     next();
+//   } catch (error) {
+//     console.error('Error verifying token:', error.message);
+//     return res.status(403).json({ error: 'Invalid or expired token' });
+//   }
+// };
+
+// module.exports= verifyToken
+
 const admin = require('firebase-admin');
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT environment variable is missing');
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
 admin.initializeApp({
-  credential: admin.credential.cert(require('../config/service_key.json')),
+  credential: admin.credential.cert(serviceAccount),
 });
+
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(403).json({ error: 'Unauthorized' });
   }
@@ -13,13 +50,16 @@ const verifyToken = async (req, res, next) => {
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     const uid = decodedToken.uid;
+
     req.user = { uid };
-    console.log("Reequesting userin the frontend is ",req.user)
+
+    console.log("Requesting user in frontend:", req.user);
+
     next();
   } catch (error) {
     console.error('Error verifying token:', error.message);
     return res.status(403).json({ error: 'Invalid or expired token' });
   }
 };
- 
-module.exports= verifyToken
+
+module.exports = verifyToken;
